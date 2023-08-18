2d4fc052fea7ea1bf9abe0390a2cf6ed88c38953
Rick Paramanik <rickkcir100@gmail.com>
2023-08-18 13:45:02 +0530

    database link changed

--- config/mongo.connection.js
+++ config/mongo.connection.js
@@ -1,3 +1,3 @@
 const mongoose = require("mongoose");
-const con = mongoose.createConnection("mongodb://localhost:27017/e_commerce");
+const con = mongoose.createConnection("mongodb+srv://rickkcir100:rick@cluster0.7mmm1xo.mongodb.net/e_commerce");
 module.exports = con;
